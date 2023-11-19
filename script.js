ab9baf85948fff784386e1c50c9c0cd1dd8b1e5f
LeonardoGangale <yolopollo97@gmail.com>
2023-11-19 18:23:48 +0100

    second commit

---
+++ script.js
@@ -0,0 +1,104 @@
+let redirect_uri = "http://127.0.0.1:5500/index.html"
+
+let client_id = "47a5f7bc78c2462e9d2a865999c42942"
+let client_secret = "2f1a872c641944fca3db0cfee225899f"
+
+const AUTHORIZE = "https://accounts.spotify.com/authorize"
+const TOKEN = "https://accounts.spotify.com/api/token"
+
+localStorage.setItem("client_id", client_id)
+localStorage.setItem("client_secret", client_secret)
+
+function onPageLoad() {
+    client_id = localStorage.getItem("client_id")
+    client_secret = localStorage.getItem("client_secret")
+
+    if (window.location.search.length > 0){
+        handleRedirect()
+    }
+}
+
+function handleRedirect() {
+    let code = getCode()
+    fetchAccessToken(code)
+    window.history.pushState("", "", redirect_uri)
+}
+
+const getCode = () => {
+    let code = null
+    const queryString = window.location.search
+    if (queryString.length > 0) {
+        const urlParams = new URLSearchParams(queryString)
+        code = urlParams.get('code')
+    }
+    return code
+}
+
+const fetchAccessToken = (code) => {
+    let body = "grant_type=authorization_code"
+    body += "&code=" + code
+    body += "&redirect_uri=" + redirect_uri
+    body += "&client_id=" + client_id
+    body += "&client_secret=" + client_secret
+    callAuthorizationApi(body)
+}
+
+function callAuthorizationApi(body){
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", TOKEN, true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.setRequestHeader('Authorization', 'Basic ' + btoa(client_id + ":" + client_secret));
+    xhr.send(body);
+    xhr.onload = handleAuthorizationResponse;
+}
+
+
+function handleAuthorizationResponse(){
+    if ( this.status == 200 ){
+        var data = JSON.parse(this.responseText);
+        console.log(data);
+        var data = JSON.parse(this.responseText);
+        if ( data.access_token != undefined ){
+            let access_token = data.access_token;
+            localStorage.setItem("access_token", access_token);
+            console.log(access_token, data)
+        }
+        if ( data.refresh_token  != undefined ){
+            let refresh_token = data.refresh_token;
+            localStorage.setItem("refresh_token", refresh_token);
+        }
+        onPageLoad();
+        goToPlayer()
+    }
+    else {
+        console.log(this.responseText);
+        alert(this.responseText);
+    }
+}
+
+const redirect = async () => {
+    let url = AUTHORIZE
+    url += "?client_id=" + client_id
+    url += "&response_type=code"
+    url += "&redirect_uri=" + encodeURI(redirect_uri)
+    url += "&show_dialog=true"
+    url += "&scope=user-read-currently-playing"
+
+    window.location.href = url;
+}
+
+function goToPlayer() {
+    window.location.href = "http://127.0.0.1:5500/spotify-player/index.html"
+}
+
+/* ---------------------------- STYLE  ----------------------------*/
+
+const highlightText = (id) => {
+    const text = document.getElementById(id)
+    text.style.color = "#1DB954"
+}
+
+const removeHighlight = (id) => {
+    const text = document.getElementById(id)
+    text.style.color = ""
+}
---
+++ spotify-player/script.js
@@ -0,0 +1,265 @@
+const TOKEN = "https://accounts.spotify.com/api/token"
+const alertText = document.getElementById("alert-text")
+const image = document.getElementById("track-image")
+const body = document.getElementsByTagName("body")[0]
+const playerContainer = document.getElementsByClassName("player-container")[0];
+const trackTitle =  document.getElementById("track-title");
+const artistsNames = document.getElementById("artists-names")
+let artistsNamesColor = "#ffffff00"
+
+function onPageLoad() {
+    window.client_id = localStorage.getItem("client_id")
+    window.client_secret = localStorage.getItem("client_secret")
+    window.authorization_code = localStorage.getItem("access_token")
+}
+
+async function makeRequest(){
+
+    let response = await fetch('https://api.spotify.com/v1/me/player/currently-playing', {
+        method: "GET",
+        headers: {
+            'Accept': 'application/json',
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + authorization_code,
+        }}).then((response) => {
+            if(response.status === 204){
+                body.style.background = "white"
+                alertText.style.display = "block"
+                playerContainer.style.display = "none"
+            } else {
+                response.json().then(
+                    (data) => { 
+                        if(response.status === 401){
+                            refreshAccessToken()
+                        }
+                        alertText.style.display = "none"
+                        playerContainer.style.display = "flex"
+                        setTrackImage(data.item.album.images[0].url)
+                        setTrackTitle(data.item.name, data.item.external_urls.spotify)
+                        setArtists(data.item.artists)
+                        setProgressbarWidth(data.progress_ms, data.item.duration_ms)
+                    }
+                );
+            }
+        })
+}
+
+function refreshAccessToken(){
+    refresh_token = localStorage.getItem("refresh_token");
+    let body = "grant_type=refresh_token";
+    body += "&refresh_token=" + refresh_token;
+    body += "&client_id=" + client_id;
+    callAuthorizationApi(body);
+}
+
+function callAuthorizationApi(body){
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", TOKEN, true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.setRequestHeader('Authorization', 'Basic ' + btoa(client_id + ":" + client_secret));
+    xhr.send(body);
+    xhr.onload = handleAuthorizationResponse;
+}
+
+function handleAuthorizationResponse(){
+    if ( this.status == 200 ){
+        var data = JSON.parse(this.responseText);
+        console.log(data);
+        var data = JSON.parse(this.responseText);
+        if ( data.access_token != undefined ){
+            access_token = data.access_token;
+            localStorage.setItem("access_token", access_token);
+        }
+        if ( data.refresh_token  != undefined ){
+            refresh_token = data.refresh_token;
+            localStorage.setItem("refresh_token", refresh_token);
+        }
+        onPageLoad();
+    }
+    else {
+        console.log(this.responseText);
+        alert(this.responseText);
+    }
+}
+
+function setTrackImage (imagePath) {
+    image.src = imagePath
+
+    let downloadedImage = new Image();
+    downloadedImage.crossOrigin = "anonymous"
+    downloadedImage.src = imagePath;
+    downloadedImage.addEventListener("load", () => {
+        let RGB = getAverageRGB(downloadedImage)
+        body.style.background = `linear-gradient(90deg, rgb(${RGB.red1}, ${RGB.green1}, ${RGB.blue1}), rgb(${RGB.avgRed}, ${RGB.avgGreen}, ${RGB.avgBlue}), rgb(${RGB.red2}, ${RGB.green2}, ${RGB.blue2}))`
+       
+        if((RGB.avgRed + RGB.avgGreen + RGB.avgBlue)/3 < 10){
+            playerContainer.style.background = `rgb(10, 10, 10, 1)`;    
+        }else{
+            playerContainer.style.background = `rgb(${RGB.avgRed}, ${RGB.avgGreen}, ${RGB.avgBlue}, 1)`;
+        }
+        
+        
+        console.log((RGB.avgRed + RGB.avgGreen + RGB.avgBlue)/3);
+        
+        if((RGB.avgRed + RGB.avgGreen + RGB.avgBlue)/3 > 150){
+            trackTitle.style.color = "black";
+            artistsNamesColor = "black"
+        } else {
+            trackTitle.style.color = "white";
+            artistsNamesColor = "white"
+        }
+    });
+}
+
+
+function calculateAverageRGB(imageData) {
+    let totalRed = 0;
+    let totalGreen = 0;
+    let totalBlue = 0;
+
+    let maxRed = 0;
+    let maxBlue = 0;
+    let maxGreen = 0;
+    let maxTreshold = 140;
+
+    let minRed = 255;
+    let minBlue = 255;
+    let minGreen = 255;
+    let minThreshold = 0;
+
+    for (let i = 0; i < imageData.data.length; i += 1024) {
+        if (imageData.data[i] > maxRed && imageData.data[i] < maxTreshold){
+            maxRed = imageData.data[i];
+        } else if (imageData.data[i] < minRed && imageData.data[i] > minThreshold){
+            minRed = imageData.data[i];
+        }
+
+        if (imageData.data[i+1] > maxGreen && imageData.data[i+1] < maxTreshold){
+            maxGreen = imageData.data[i+1];
+        }else if (imageData.data[i+1] < minGreen && imageData.data[i+1] > minThreshold){
+            minGreen = imageData.data[i+1];
+        }
+
+        if (imageData.data[i+2] > maxBlue && imageData.data[i+2] < maxTreshold){
+            maxBlue = imageData.data[i+2];
+        } else if (imageData.data[i+2] < minBlue && imageData.data[i+2] > minThreshold){
+            minBlue = imageData.data[i+2];
+        }
+
+      totalRed += imageData.data[i];
+      totalGreen += imageData.data[i + 1];
+      totalBlue += imageData.data[i + 2];
+    }
+
+    console.log(minRed, minGreen, minBlue)
+    const pixelCount = imageData.data.length / 1024;
+    const averageRed = totalRed / pixelCount;
+    const averageGreen = totalGreen / pixelCount;
+    const averageBlue = totalBlue / pixelCount;
+
+    return {
+      red1: maxRed,
+      green1: maxGreen,
+      blue1: maxBlue,
+      
+      red2: minRed,
+      green2: minGreen,
+      blue2: minBlue,
+
+      avgRed: averageRed,
+      avgGreen: averageGreen,
+      avgBlue: averageBlue,
+    };
+}
+
+function getAverageRGB(image) {
+    const imageElement = image;
+    console.log(imageElement)
+
+    const canvas = document.createElement('canvas');
+    const context = canvas.getContext('2d');
+
+
+    canvas.width = imageElement.width;
+    canvas.height = imageElement.height;
+
+    context.drawImage(imageElement, 0, 0, imageElement.width, imageElement.height);
+
+    const imageData = context.getImageData(0, 0, imageElement.width, imageElement.height);
+    return calculateAverageRGB(imageData);
+};
+
+function setTrackTitle (title, linkToTrack) {
+    trackTitle.innerHTML = title
+    trackTitle.href = linkToTrack
+
+}
+
+function setArtists(namesObject){
+    while(artistsNames.firstChild) {
+        artistsNames.removeChild(artistsNames.firstChild)
+    }
+    
+    let counter = 0
+    namesObject.forEach(element => {
+    
+        let link = document.createElement("a")
+        link.style.cssText = "color: " + artistsNamesColor + "; font-size: 25px; font-family: 'Circular'; text-decoration: none"
+
+        link.addEventListener('mouseenter', () => {
+            link.style.textDecoration = "underline"
+            link.style.textDecorationColor = artistsNamesColor
+        })
+    
+        link.addEventListener('mouseleave', () => {
+            link.style.textDecoration = "none"
+        })
+
+
+        let name = document.createElement("p")
+        name.style.cssText = "color: 'black'; margin: 0"
+        
+        name.innerHTML = element.name
+        if(namesObject.length > 1  && counter < namesObject.length - 1 ){
+            name.innerHTML += ",&nbsp"
+        }
+
+        
+        link.href = element.external_urls.spotify
+        link.target = "_blank"
+    
+        link.appendChild(name)
+        artistsNames.appendChild(link)
+        
+        counter ++
+    })
+}
+
+function convertMillisecond(value){
+    let result = ""
+    let minutes = Math.floor(value/60000)
+    let seconds = Math.floor((value - minutes * 60000) / 1000)
+    if (seconds < 10){
+        result = `${minutes} : 0${seconds}`
+    } else {
+        result = `${minutes} : ${seconds}`
+    }
+
+    return result
+}
+
+const progressBar = document.getElementById("progress-foreground")
+const durationText =  document.getElementById("dt-text")
+const progressText =  document.getElementById("pg-text")
+
+function setProgressbarWidth(progress, duration){
+    progressBar.style.width = `${progress / duration * 100}%`
+    durationText.innerHTML =  convertMillisecond(duration)
+    progressText.innerHTML = convertMillisecond(progress)
+}
+
+function setVolume(percentage){
+    console.log(percentage, "%")
+}  
+
+setInterval(async () => {makeRequest()}, 1000)
